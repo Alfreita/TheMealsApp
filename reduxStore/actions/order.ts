722c5301d80c46b8637a4d3df0bee3e07c10c087
Victor Almeida de Freitas <victor.freitas08@hotmail.com>
2021-07-12 08:47:21 -0300

    adding orders

--- reduxStore/actions/order.ts
+++ reduxStore/actions/order.ts
@@ -1,8 +1,37 @@
 export const ADD_ORDER = "ADD_ORDER";
 
-export const addOrder = (cartItem: any, totalAmount: any) => {
-  return {
-    type: ADD_ORDER,
-    orderData: { items: cartItem, amount: totalAmount },
-  };
+export const addOrder = (cartItems: any, totalAmount: any) => {
+  // return {
+  //   type: ADD_ORDER,
+  //   orderData: { items: cartItem, amount: totalAmount },
+  // };
+  try {
+    const data = new Date();
+    return async (dispatch: any) => {
+      const response = await fetch(
+        "https://theshopapp-2071e-default-rtdb.firebaseio.com/orders/u1.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            cartItems,
+            totalAmount,
+            date: data.toISOString(),
+          }),
+        }
+      );
+      const resData = await response.json();
+      dispatch({
+        type: ADD_ORDER,
+        orderData: {
+          id: resData.name,
+          items: cartItems,
+          amount: totalAmount,
+          date: data,
+        },
+      });
+    };
+  } catch (error) {}
 };
